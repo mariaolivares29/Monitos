efa93743554ed8d6754aa10e3493438a02bf2464
mariaolivares29 <113628788+mariaolivares29@users.noreply.github.com>
2022-11-16 23:35:24 -0400

    Add files via upload

---
+++ Monitos.js
@@ -0,0 +1,11 @@
+const aSonrisa = false;
+const bSonrisa = false;
+const alerta = "Es cierto que los monitos están en problemas"
+If (aSonrisa == false && bSonrisa == false || aSonrisa == true && bSonrisa == true)
+{
+    console.log(alerta)
+}
+else
+{
+    console.log("Es falso que los monitos están en problemas");
+}
